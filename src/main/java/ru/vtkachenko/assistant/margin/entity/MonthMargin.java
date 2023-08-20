b232c070ee79a70118b6ad3c082d45f1d2b9a7ca
TkachenkoVitaliy <tkachenko.java@yandex.ru>
2023-08-20 12:42:45 +0500

    work on margin domain added files

---
+++ src/main/java/ru/vtkachenko/assistant/margin/entity/MonthMargin.java
@@ -0,0 +1,33 @@
+package ru.vtkachenko.assistant.margin.entity;
+
+import com.poiorm.annotation.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@RowObject(startRowIndex = 6)
+public class MonthMargin {
+    @ExcelCell(0)
+    @IdentifierField
+    private String month;
+    @ExcelCell(3)
+    private double margin;
+    @InnerRowObject
+    private List<CityMargin> cities = new ArrayList<>();
+
+    @IdentifierMethod
+    public static boolean check(String value) {
+        if (value == null) return false;
+        List<String> months = List.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12");
+        String summary = "Итого";
+        return months.contains(value) || summary.equals(value);
+    }
+}
---
+++ src/main/java/ru/vtkachenko/assistant/margin/entity/SummaryCityMargin.java
@@ -0,0 +1,32 @@
+package ru.vtkachenko.assistant.margin.entity;
+
+import com.poiorm.annotation.ExcelCell;
+import com.poiorm.annotation.IdentifierField;
+import com.poiorm.annotation.IdentifierMethod;
+import com.poiorm.annotation.RowObject;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@RowObject(startRowIndex = 5)
+public class SummaryCityMargin {
+    @ExcelCell(0)
+    @IdentifierField
+    private String city;
+    @ExcelCell(3)
+    private double margin;
+
+    @IdentifierMethod
+    public static boolean check(String value) {
+        if (value == null) return false;
+        System.out.println("Check " + value);
+        return value.length() > 2 && value.length() < 20;
+    }
+}
