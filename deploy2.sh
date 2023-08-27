752fe073f8c04dc552d9a9927d3ebd91bf81d5c3
TkachenkoVitaliy <tkachenko.java@yandex.ru>
2023-08-27 12:51:55 +0500

    v2 cicd

---
+++ deploy2.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+cd application/backend/assistant
+mvn -s settings.xml clean install -DskipTests
+mv target/assistant-*.jar target/assistant.jar
+mv JenkinsDockerfile target/JenkinsDockerfile
+docker rm --force back
+docker rmi --force backend
+cd target
+docker build -t backend -f JenkinsDockerfile .
+docker run -d -it -p 8089:8089 --restart always --name back backend
+docker network inspect dockernet >/dev/null 2>&1 || docker network create --driver bridge dockernet
+docker network connect dockernet back
